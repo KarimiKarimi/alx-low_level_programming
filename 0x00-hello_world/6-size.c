49b2133d2ee7f0f1d4a56feaf8ad612c48f369b5
KarimiKarimi <karimigrace08@gmail.com>
2023-09-09 12:33:23 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,4 +6,6 @@ int main (void)
         printf("size of long int %li byte(s)\n",sizeof(long int));
          printf("size of long long int is %lli byte(s)\n",sizeof(long long int));
 	printf("size of float is %f byte(s)\n",sizeof(float));
+ return (0);
 }
+
