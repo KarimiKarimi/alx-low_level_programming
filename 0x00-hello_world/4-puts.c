658ef3fd2c331368edc9f270591af5323577506e
KarimiKarimi <karimigrace08@gmail.com>
2023-09-11 15:00:38 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main-entry point
+ * main-a program that prints a line using puts funtion
+ * return:0(success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
